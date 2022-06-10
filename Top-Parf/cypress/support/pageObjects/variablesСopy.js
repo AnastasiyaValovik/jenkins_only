d5ba3c9e7055038ea4d23c0e926d80b24f66b6de
anastasiya.valovik <anastasiya.volovik@intexsoft.by>
2022-06-10 16:35:04 +0300

    first commit

---
+++ Top-Parf/cypress/support/pageObjects/variablesСopy.js
@@ -0,0 +1,55 @@
+
+export class Variables1 {
+
+    mainPageTitle = 'Parfum, Make-up, Pflegeprodukte online kaufen | parfuemerie.de'
+
+    markenPageTitle = 'Kosmetikmarken online kaufen | parfuemerie.de'
+
+    duftePageTitle = 'Düfte & Parfum online kaufen | parfuemerie.de'
+
+    pflegePageTitle = 'Hautpflege Produkte online kaufen | Pflegeprodukte | parfuemerie.de'
+
+    makeupPageTitle = 'Make-up online kaufen | Make up bei | parfuemerie.de'
+
+    haarePageTitle = 'Haarpflege & Haarstyling Produkte online kaufen | Haarpflege | parfuemerie.de'
+
+    accessoiresPageTitle = 'Accessoires | Raumdüfte & Duftkerzen | parfuemerie.de'
+
+    beautyFoodPageTitle = 'Nahrungsergänzung online kaufen | Parfümerie Wiedemann'
+
+    salePageTitle = '% Beauty SALE % Parfum & Kosmetik online kaufen | parfuemerie.de'
+
+    gratisPageTitle = 'Exklusive Gratis-Geschenke | Gratis Zugaben für Sie - parfuemerie.de'
+
+    genussweltPageTitle = 'Feinkost online kaufen | parfuemerie.de'
+
+    storefinderPageTitle = 'STOREFINDER - parfuemerie.de'
+
+    //SERVICE page can't be open
+
+    cartPageTitle = 'Warenkorb'
+
+    searchResultsTitle = 'Suchergebnisse für:' //Title consist of 'Suchergebnisse für:' string + string your were searching for
+
+   
+    productFromCategory = 'Tom Ford Noir E.d.P. Nat. Spray'
+
+    productFromSearchBox = 'Estée Lauder Double Wear Maximum Cover Camouflage Makeup for Face and Body SPF 15'
+
+   
+    email = 'sivuto.kifipoyo@intexsoft.by'
+
+    password = 'magento2'
+
+    firstName = 'Anna'
+
+    surName = 'Fry'
+
+    unregisteredName = 'Maks'
+
+    unregisteresSurName = 'First'
+
+
+}
+
+export const variables = new Variables1() 
